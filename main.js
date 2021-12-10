833f686a0bdab1db255df6cbdfbce88bfd49a83f
Light Lee <43360748+viptomsu@users.noreply.github.com>
2021-12-10 16:49:22 +0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,63 +1,47 @@
-function toast({
-   title = '',
-   msg = '',
-   type = 'info',
-   duration = 3000
-}) {
-   const main = document.getElementById('toasts');
-   if (main) {
-      const toast = document.createElement('div');
-      const icons = {
-         success: "fa-solid fa-circle-check",
-         info: "fa-solid fa-circle-info",
-         warning: "fa-solid fa-circle-exclamation",
-         danger: "fa-solid fa-circle-radiation"
-      }
-      const fadeOutTime = 400;
-      toast.classList.add('toast', `toast--${type}`);
-      toast.style.animation = `slideIn 0.3s ease-in-out, fadeOut ${fadeOutTime/1000}s linear ${duration/1000}s forwards`;
-      toast.innerHTML = `
-         <div class="toast__icon">
-            <i class="${icons[type]}"></i>
-         </div>
-         <div class="toast__body">
-            <h3 class="toast__title">${title}</h3>
-            <p class="toast__msg">${msg}</p>
-         </div>
-         <div class="toast__close">
-            <i class="fa-solid fa-xmark"></i>
-         </div>
-      `;
-      main.appendChild(toast);
-      setTimeout(function () {
-         main.removeChild(toast);
-      }, duration + fadeOutTime + 400)
+const $ = document.querySelector.bind(document);
+const $$ = document.querySelectorAll.bind(document);
+
+function slideHandler(duration) {
+   const tabs = $$('.tab-item');
+   const tabContents = $$('.tab-content');
+   console.log(tabs);
+
+   function underline(activeTab) {
+      const line = $('.line');
+      line.style.left = activeTab.offsetLeft + 'px';
+      line.style.width = activeTab.offsetWidth + 'px';
    }
-}
-function btnClicked(btns,
-   titleParameter = '',
-   msgParameter = '',
-   typeParameter = 'info',
-   durationParameter = 1000
-) {
-   for (const btn of btns) {
-      btn.onclick = function () {
-         toast({
-            title: titleParameter,
-            msg: msgParameter,
-            type: typeParameter,
-            duration: durationParameter
-         })
-      }
+   
+   function activeTab(index) {
+      const tabContent = tabContents[index];
+      $('.tab-item.active').classList.remove('active');
+      $('.tab-content.active').classList.remove('active');
+      tabs[index].classList.add('active');
+      tabContent.classList.add('active');
+      underline(tabs[index]);
    }
+
+   underline($('.tab-item.active'));
+
+   // click tab
+   tabs.forEach((tab, index) => {
+      tab.onclick = function() {
+         activeTab(index);
+      }
+   })
+
+   // slide auto run
+   setInterval(() => {
+      for(let i = 0; i < tabs.length; i++) {
+         if(tabs[i].classList.contains('active')){
+            if(++i < tabs.length) {
+               activeTab(i);
+            } else activeTab(0);
+            break;
+         }
+      }
+   }, duration*1000);
 }
 
-const btnSuccess = document.getElementsByClassName('btn--success');
-const btnInfo = document.getElementsByClassName('btn--info');
-const btnWarning = document.getElementsByClassName('btn--warning');
-const btnDanger = document.getElementsByClassName('btn--danger');
+slideHandler(2);
 
-btnClicked(btnSuccess, 'Success', 'Your tickets will be sent to you soon', 'success', 1000);
-btnClicked(btnInfo, 'Information', 'New informations', 'info', 1000);
-btnClicked(btnWarning, 'Warning', 'This action may harm your computer', 'warning', 1000);
-btnClicked(btnDanger, 'Error', 'Please try again later', 'danger', 1000);
